1ab1b045a1896016690c1e16c491a030883edc70
kradova <ksenia.radova@gmail.com>
2022-07-07 23:36:12 +0300

    + todo items to list in DOM

--- app-todo/app.js
+++ app-todo/app.js
@@ -1,15 +1,49 @@
+const DB = `[
+    {
+        "id": 1,
+        "title": "ToDo1",
+        "isDone": false
+    },
+    {
+        "id": 2,
+        "title": "ToDo2",
+        "isDone": false
+    },
+    {
+        "id": 3,
+        "title": "ToDo3",
+        "isDone": false
+    }
+]`;
+
+let todos = getData();
+//console.log(todos);
+
+
+todos.forEach(item => {
+    addToList(item);
+});
+
+
+
 function getData() {
-    
+    return JSON.parse(DB);
 }
 
 function addToList(item) {
-    
-}
+    //console.log(item);
+    let list = $("#list");
+    let newTodo = $("#item-template").html();
+    newTodo = newTodo.replace(/\{id\}/g, item.id).replace("{title}", item.title);
+    console.log(newTodo);
+    list.append(newTodo);
+}// addToList
 
 function deleteItem(itemID) {
-    
+
 }
 
 function changeIsDone(itemID) {
-    
+
 }
+

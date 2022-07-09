9533edfefd29ddc8c78b00b30a13d2cd9130eb2c
kradova <ksenia.radova@gmail.com>
2022-07-10 00:01:54 +0300

    + all fields of item to list

--- app-todo/app.js
+++ app-todo/app.js
@@ -2,7 +2,7 @@ const DB = `[
     {
         "id": 1,
         "title": "ToDo1",
-        "isDone": "false"
+        "isDone": "true"
     },
     {
         "id": 2,
@@ -35,6 +35,11 @@ function addToList(item) {
     let list = $("#list");
     let newTodo = $("#item-template").html();
     newTodo = newTodo.replace(/\{id\}/g, item.id).replace("{title}", item.title);
+    if (item.isDone === "true") {
+        newTodo = newTodo.replace("{isdone}", "checked");
+    } else {
+        newTodo = newTodo.replace("{isdone}", "");
+    }
     console.log(newTodo);
     list.append(newTodo);
 }// addToList

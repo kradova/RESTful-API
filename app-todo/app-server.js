b68be645a37ed432f3b73fd666febbaaa24ad090
kradova <ksenia.radova@gmail.com>
2022-07-31 17:46:59 +0300

    + update todo

--- app-todo/app-server.js
+++ app-todo/app-server.js
@@ -4,13 +4,13 @@ getData();
 
 /** Change isDone
 *=================*/
-$(".form-check-input").on("click", (event) => {
+$("#list").on("change", ".form-check-input", (event) => {
     let element = event.target;
     let itemID = $(element).data("id");
     
-    const url = "http://localhost:3000/todos/" + itemID;
+    const urlTodos = "http://localhost:3000/todos/" + itemID;
     const method = "PUT";
-    let newIsDone;
+    let itemToUpdate = {};
     todos.forEach(item => {
         if (itemID == item.id) {
             if (item.isDone === "true") {
@@ -19,12 +19,10 @@ $(".form-check-input").on("click", (event) => {
                 item.isDone = "true";
                 //$(element).parrent.css('text-decoration', 'line-through');
             }
-            newIsDone = item.isDone;
+            itemToUpdate = item;
         }
     });
-    let newData = {
-        isDone: newIsDone
-    };
+    let newData = itemToUpdate;
     $.ajax({
         url: urlTodos,
         method: method,
